0d44eed75b8a70d6793d3933c42bdd527357dcfc
Whiteline <48377708+xwhiteex@users.noreply.github.com>
2021-10-28 18:42:17 +0300

    Update automatr.ps1

--- automatr.ps1
+++ automatr.ps1
@@ -1,39 +1,83 @@
-Write-Host "Welcome to the Automatiz-inator"
-Write-Host "This script activates Windows & installs essential software for Productivity"
+Write-Host "Welcome to the Automatiz-inator."
+Write-Host "This script activates Windows & installs some software."
 
 # Windows Activation
-slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
-slmgr /skms kms8.msguides.com
-slmgr /ato
+$id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$p = New-Object System.Security.Principal.WindowsPrincipal($id)
+if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+    slmgr /skms kms8.msguides.com
+    slmgr /ato
+} else {
+    Write-Host "Please run the script as an Administrator to activate Windows."
+}
 
 # Audacity
-winget install Audacity.Audacity
+$confirm01 = Read-Host "Do you wish to install Audacity on your computer? (y/n)"
+if($confirm01 -eq "y") {
+    winget install Audacity.Audacity
+}
 
 # Blender
-winget install BlenderFoundation.Blender
+$confirm02 = Read-Host "Do you wish to install Blender on your computer? (y/n)"
+if($confirm02 -eq "y") {
+    winget install BlenderFoundation.Blender
+}
 
 # Discord
-winget install Discord.Discord
+$confirm03 = Read-Host "Do you wish to install Discord on your computer? (y/n)"
+if($confirm03 -eq "y") {
+    winget install Discord.Discord
+}
 
 # GitHub
-winget install Git.Git
-winget install GitHub.GitLFS
-winget install GitHub.GitHubDesktop
+$confirm04 = Read-Host "Do you wish to install Git & GitHub Desktop on your computer? (y/n)"
+if($confirm04 -eq "y") {
+    winget install Git.Git
+    winget install GitHub.GitLFS
+    winget install GitHub.GitHubDesktop
+}
 
 # Notepad
-winget install Notepad++.Notepad++
+$confirm05 = Read-Host "Do you wish to install Notepad++ on your computer? (y/n)"
+if($confirm05 -eq "y") {
+    winget install Notepad++.Notepad++
+}
 
 # NodeJS
-winget install OpenJS.NodeJS
+$confirm06 = Read-Host "Do you wish to install NodeJS on your computer? (y/n)"
+if($confirm06 -eq "y") {
+    winget install OpenJS.NodeJS
+}
 
 # OBS
-winget install OBSProject.OBSStudio
+$confirm07 = Read-Host "Do you wish to install OBS Studio on your computer? (y/n)"
+if($confirm07 -eq "y") {
+    winget install OBSProject.OBSStudio
+}
 
 # Parsec
-winget install Parsec.Parsec
+$confirm08 = Read-Host "Do you wish to install Parsec on your computer? (y/n)"
+if($confirm08 -eq "y") {
+    winget install Parsec.Parsec
+}
 
 # Rainmeter
-winget install Rainmeter
+$confirm09 = Read-Host "Do you wish to install Rainmeter on your computer? (y/n)"
+if($confirm09 -eq "y") {
+    winget install Rainmeter
+}
+
+# StartAllBack (for Windows 11)
+if ([System.Environment]::OSVersion.Version.Build -ge 22000) {
+    $confirm10 = Read-Host "Do you wish to install StartAllBack on your computer? (y/n)"
+    if($confirm10 -eq "y") {
+        winget install startallback
+    }
+}
 
 # VLC
-winget install VideoLAN.VLC
+$confirm11 = Read-Host "Do you wish to install VLC on your computer? (y/n)"
+if($confirm11 -eq "y") {
+    winget install VideoLAN.VLC
+}
