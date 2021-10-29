77ddb8ac0533954524a461c41a176750cbc5cbd0
Whiteline <48377708+xwhiteex@users.noreply.github.com>
2021-10-29 03:18:23 +0300

    Update automatr.ps1

--- automatr.ps1
+++ automatr.ps1
@@ -1,21 +1,24 @@
 Write-Host '=====> automatr <====='
 
 # Windows Activation
-$id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$p = New-Object System.Security.Principal.WindowsPrincipal($id)
-if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
-    Start-Sleep -Seconds 3
-    slmgr /skms kms8.msguides.com
-    Start-Sleep -Seconds 3
-    slmgr /ato
-    Write-Host "==================================================================="
-    Write-Host "The OS was activated successfully."
-    Write-Host "==================================================================="
-} else {
-    Write-Host "==================================================================="
-    Write-Host "Please run the script as an Administrator to activate Windows."
-    Write-Host "==================================================================="
+$confirm00 = Read-Host 'Would you like to activate your version of Windows? (y/n)'
+if($confirm00 -eq "y") {
+    $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $p = New-Object System.Security.Principal.WindowsPrincipal($id)
+    if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+        Start-Sleep -Seconds 3
+        slmgr /skms kms8.msguides.com
+        Start-Sleep -Seconds 3
+        slmgr /ato
+        Write-Host "==================================================================="
+        Write-Host "The OS was activated successfully."
+        Write-Host "==================================================================="
+    } else {
+        Write-Host "==================================================================="
+        Write-Host "Please run the script as an Administrator to activate Windows."
+        Write-Host "==================================================================="
+    }
 }
 
 # Audacity
